99074428780d5d268268a04e0ebb7361db3b5c3f
Josh Rule <rule@mit.edu>
2016-06-14 11:10:37 -0400

    first implementation of a world state

--- LOTlib/Projects/CupStacking/play.py
+++ LOTlib/Projects/CupStacking/play.py
@@ -5,47 +5,128 @@
 
 from LOTlib.Grammar import Grammar
 
-grammar = Grammar(start='ACTION')
+grammar = Grammar(start='WORLD-STATE')
 
-grammar.add_rule('ACTION', 'stack_',           ['OBJECT', 'OBJECT'], 1.0)
-grammar.add_rule('ACTION', 'if_',              ['BOOL', 'ACTION', 'ACTION'], 1.0)
-grammar.add_rule('ACTION', 'noop_',            [], 1.0)
-# grammar.add_rule('OBJECT', 'choose_random_',   ['SET'], 1.0)
+# !! Note the types have changed since play1
+grammar.add_rule('WORLD-STATE', '%s.stack()',           ['WORLD-STATE'], 1.0)
+grammar.add_rule('WORLD-STATE', '%s.left_grab(%s)',    ['WORLD-STATE', 'OBJECT'], 1.0)
+grammar.add_rule('WORLD-STATE', '%s.right_grab(%s)',    ['WORLD-STATE', 'OBJECT'], 1.0)
+grammar.add_rule('WORLD-STATE', '%s.left_drop()',    ['WORLD-STATE'], 1.0)
+grammar.add_rule('WORLD-STATE', '%s.right_drop()',    ['WORLD-STATE'], 1.0)
+
+grammar.add_rule('WORLD-STATE',   'ws',    None, 10.0)
+grammar.add_rule('WORLD-STATE', 'if_',    ['BOOL', 'WORLD-STATE', 'WORLD-STATE'], 1.0)
+grammar.add_rule('WORLD-STATE', 'recurse_',         ['WORLD-STATE'], 1.0)
 
 grammar.add_rule('BOOL',   '(%s > %s)',              ['SIZE', 'SIZE'], 1.0)
 grammar.add_rule('BOOL',   '(%s == %s)',              ['SIZE', 'SIZE'], 1.0)
 
-grammar.add_rule('SIZE',   '%s.top_size',        ['OBJECT'], 1.0)
-grammar.add_rule('SIZE',   '%s.bottom_size',     ['OBJECT'], 1.0)
-grammar.add_rule('SIZE',   '%s.height',          ['OBJECT'], 1.0) # number of parts
+grammar.add_rule('SIZE',   'getattr(%s,"top_size",None)',        ['OBJECT'], 1.0)
+grammar.add_rule('SIZE',   'getattr(%s,"bottom_size",None)',     ['OBJECT'], 1.0)
+grammar.add_rule('SIZE',   'getattr(%s,"height",None)',          ['OBJECT'], 1.0) # number of parts
 
-grammar.add_rule('OBJECT', 'x', None, 1.0)
-grammar.add_rule('OBJECT', 'y', None, 1.0)
+
+grammar.add_rule('OBJECT', 'ws.choose_random()', None, 1.0)
+# grammar.add_rule('OBJECT', '%s.choose_random()', ['WORLD-STATE'], 1.0)
+grammar.add_rule('OBJECT', '%s.left_hand', ['WORLD-STATE'], 1.0)
+grammar.add_rule('OBJECT', '%s.right_hand', ['WORLD-STATE'], 1.0)
+
+# grammar.add_rule('SET', '%s.table', ['WORLD-STATE'], 5.0)
+# grammar.add_rule('SET', 'diff_', ['SET', 'SET'], 1.0)
+# grammar.add_rule('SET', 'set([%s])', ['OBJECT'], 1.0)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Cups
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+class WorldException(Exception):
+    pass
+
 class Cup(object):
     def __init__(self, top_size=0, bottom_size=1, height=1):
         self.__dict__.update(locals())
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Primitives
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class WorldState(object):
+    """
+    Capture the world state. By default, everything should return a worldstate (self is okay)
 
-from LOTlib.Eval import primitive
+    """
 
-@primitive
-def stack_(x,y): # put y on top of x
-    if (x is not y) and (y.bottom_size <= x.top_size): # can't put something on top of itself:
-        return Cup(top_size=y.top_size, bottom_size=x.bottom_size, height=x.height + y.height)
-    else:
-        return None
+    def __init__(self):
+        self.table =  set([Cup(top_size=n+1, bottom_size=n, height=1) for n in xrange(10)])
+        self.left_hand  = None
+        self.right_hand = None
 
-@primitive
-def noop_():
-    return None
+    def __str__(self):
+        return '[%s, %s, %s]'  % (self.left_hand, self.right_hand, str([x.height for x in self.table]))
+
+    def left_grab(self, x):
+        if x in self.table and x is not None:
+
+            if self.left_hand is not None:
+                self.left_drop()
+
+            self.left_hand = x
+
+            self.table.remove(x)
+        else:
+            raise WorldException
+
+        return self
+
+
+    def right_grab(self, x):
+        if x in self.table and x is not None:
+
+            if self.right_hand is not None:
+                self.right_drop()
+
+            self.right_hand = x
+
+            self.table.remove(x)
+        else:
+            raise WorldException
+
+        return self
+
+    def left_drop(self):
+        if self.left_hand is not None:
+            self.table.add(self.left_hand)
+
+        self.left_hand = None
+
+        return self
+
+    def right_drop(self):
+
+        if self.right_hand is not None:
+            self.table.add(self.right_hand)
+
+        self.right_hand = None
+
+        return self
+
+    def stack(self):
+        # Put right hand on top of the left
+
+        x, y = self.left_hand, self.right_hand
+
+        if x is not None and y is not None and (y.bottom_size <= x.top_size):  # can't put something on top of itself:
+            c = Cup(top_size=y.top_size, bottom_size=x.bottom_size, height=x.height + y.height)
+
+            self.left_hand = c
+            self.right_hand = None
+        else:
+            raise WorldException
+
+        return self
+
+    def choose_random(self):
+        if len(self.table) > 1:
+            return sample1(self.table)
+        else:
+            raise WorldException
+
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -55,72 +136,51 @@ def noop_():
 from math import log
 from LOTlib import break_ctrlc
 from LOTlib.Miscellaneous import sample1, Infinity, q, attrmem
-from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis
-from LOTlib.Eval import TooBigException
+from LOTlib.Hypotheses.RecursiveLOTHypothesis import RecursiveLOTHypothesis
+from LOTlib.Eval import TooBigException, RecursionDepthException
 
-class StackerHypothesis(LOTHypothesis):
+class StackerHypothesis(RecursiveLOTHypothesis):
 
     def __init__(self, grammar=grammar, **kwargs):
-        LOTHypothesis.__init__(self, grammar=grammar, display='lambda x, y: %s', **kwargs)
+        RecursiveLOTHypothesis.__init__(self, grammar=grammar, display='lambda recurse_, ws: %s', **kwargs) # for recursive hypotheses, must pass in recurses
 
     @attrmem('likelihood')
     def compute_likelihood(self, data, shortcut=None):
         assert len(data) == 0
 
-        ll = 0
+        ws = WorldState()
 
-        NRUNS = 100
-        MAX_IT = 1000
+        try:
+            ws = self(ws)
 
-        # Average over a bunch of runs
-        for _ in xrange(NRUNS):
+        except (TooBigException, RecursionDepthException):
+            return -100
+        except WorldException:
+            pass
 
-            S = [Cup(top_size=n+1, bottom_size=n, height=1) for n in xrange(10)]
-            it = 0
-
-            while len(S) > 1:
-                x, y = sample1(S), sample1(S)
-
-                # call our function
-                try:
-                    r = self(x,y)
-                except TooBigException:
-                    r = None
-
-                if r is not None:
-
-                    S.remove(x) # remove from S
-                    if y in S: # because it might be removed
-                        S.remove(y)
-
-                    S = S + [r]
-
-                if it >  MAX_IT:
-                    ll += max([x.height for x in S]) - log(MAX_IT)
-                    break
-
-                it += 1
-
-            ll += S[0].height - log(it)
-
-        return ll
+        return max([x.height for x in list(ws.table)] + [getattr(ws.left_hand, "height", 0),
+                                                         getattr(ws.right_hand, "height", 0)])
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Hypothesis
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# for _ in xrange(100):
+# for _ in xrange(1000):
 #     h = StackerHypothesis()
-#
+#     # print q(h)
 #     print h.compute_likelihood([]), q(h)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Sampling
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+#
 from LOTlib.Inference.Samplers.MetropolisHastings import MHSampler
 
 h0 = StackerHypothesis()
 
-for h in break_ctrlc(MHSampler(h0, [], steps=100000, skip=100)):
+# we'll run with a low likelihood_temperature to count the data more
+
+for h in break_ctrlc(MHSampler(h0, [], steps=100000, skip=100, prior_temperature=1.0, likelihood_temperature=0.01)):
     print h.posterior_score, h.prior, h.likelihood, q(h)
+
+
