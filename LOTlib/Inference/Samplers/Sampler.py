451d37378f484da9aa7a7856c308961ca35bcebd
Josh Rule <rule@mit.edu>
2017-09-15 16:47:31 -0400

    rework Sampler.py

--- LOTlib/Inference/Samplers/Sampler.py
+++ LOTlib/Inference/Samplers/Sampler.py
@@ -5,9 +5,11 @@ from math import log, exp, isnan
 from random import random
 
 
-def MH_acceptance(cur, prop, fb, p=None, acceptance_temperature=1.0):
+def MH_acceptance(cur, prop, fb, p=None, temperature=1.0):
     """
-    Returns whether to accept the proposal, while handling weird corner cases for computing MH acceptance ratios.
+    decides whether to accept a proposal while handling corner cases
+
+    Corner case handling is motivated by computing MH acceptance ratios.
 
     Parameters
     ----------
@@ -17,32 +19,37 @@ def MH_acceptance(cur, prop, fb, p=None, acceptance_temperature=1.0):
         The proposal's posterior score
     fb : float
         The forward-backward ratio
-    p : float or None
+    p : float or None, optional
         If a float is specified, this is the random sample drawn
-    acceptance_temperature : float
+    temperature : float, optional
         What is the temperature of this acceptance?
+    Returns
+    -------
+    bool
+        True if we accept and False if we reject
     """
-    # If we get infs or are in a stupid state, let's just sample from the prior so things don't get crazy
+    # If we get infs or are in a stupid state, just sample from the prior
     if isnan(cur) or (cur == -Infinity and prop == -Infinity):
-        # Just choose at random -- we can't sample priors since they may be -inf both
+        # choose at random -- don't sample priors since they may both be -inf
         r = -log(2.0)
 
-    elif isnan(prop) or prop==-Infinity or fb == Infinity:
-        return False # never accept
+    elif isnan(prop) or prop == -Infinity or fb == Infinity:
+        # never accept
+        return False
 
     else:
-        r = (prop-cur-fb) / acceptance_temperature
+        r = (prop-cur-fb) / temperature
 
     # And flip unless we supplied the p
-    return r >= 0.0 or (p<exp(r) if p is not None else random() < exp(r))
+    return r >= 0.0 or ((p if p is not None else random()) < exp(r))
 
 
 class Sampler(object):
     """
-    Sampler class template. Generator format, call __iter__() or next() to yield more samples.
+    Sampler class template in generator format
 
-    'States' for the sampler refer to the most recently yielded sample.
-
+    - call __iter__() or next() to yield more samples.
+    - 'States' for the sampler refer to the most recently yielded sample.
     """
 
     def __init__(self):
@@ -85,7 +92,8 @@ class Sampler(object):
 
     def compute_posterior(self, h, data, shortcut=-Infinity):
         """
-        A wrapper for hypothesis.compute_posterior(data) that can be overwritten in fancy subclassses.
+        A wrapper for hypothesis.compute_posterior(data).
+        It can be overwritten in fancy subclassses.
         """
         self.posterior_calls += 1
         return h.compute_posterior(data, shortcut=shortcut)
