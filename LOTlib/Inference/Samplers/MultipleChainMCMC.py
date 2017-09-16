a85fcf104aa97cad3807bb0529cd7580f6a8ddc8
Josh Rule <rule@mit.edu>
2017-09-16 11:42:54 -0400

    clean up MultipleChainMCMC.py

--- LOTlib/Inference/Samplers/MultipleChainMCMC.py
+++ LOTlib/Inference/Samplers/MultipleChainMCMC.py
@@ -1,44 +1,73 @@
 """
-    Multiple parallel chains running at once.
-
-    This runs skip steps within each chain, looping to return samples in a roundrobin fashion,
-    chain1, chain2, chain3, ...
-
-    Also, steps is the *total* number of steps, not the number of steps for each chain
-    This is subclassed by several other inference techniques
+Multiple parallel MCMC chains running at once.
 """
 from copy import copy
-
 from LOTlib.Miscellaneous import Infinity
 from LOTlib.Inference.Samplers.MetropolisHastings import MHSampler
-from LOTlib.Inference.Samplers import Sampler
+from LOTlib.Inference.Samplers.Sampler import Sampler
+
 
 class MultipleChainMCMC(Sampler):
-    
-    def __init__(self, make_h0, data, steps=Infinity, nchains=10, make_sampler=None, **kwargs):
-        """
-        :param make_h0: -- a function to make h0 for each chain
-        :param data:  -- what data we use
-        :param steps:  -- how many steps (total, across all chains)
-        :param nchains:  -- how many chains
-        :param make_sampler: -- a function that takes make_h0, data, and steps
-        :param kwargs: -- special args to sampler
-        :return:
-        """
+    """
+    Multiple parallel MCMC chains running at once.
+
+    Run skip steps within each chain, looping to return samples in a roundrobin
+    fashion, chain1, chain2, chain3, ...
+
+    This is subclassed by several other inference techniques
+
+    Parameters
+    ----------
+    h0 : LOTlib.Hypotheses.Hypothesis
+        the initial hypothesis, used in each chain
+    data : list of data
+        what data we use
+    steps : int
+        how many *total* steps the sampler takes, not the number for each chain
+    nchains : int
+        how many chains
+    **kwargs :
+        special args to sampler
+    """
+
+    def __init__(self, h0, data, steps=Infinity, nchains=10, **kwargs):
 
         self.nchains = nchains
-        self.chain_idx = -1 # what chain are we on? This get incremented before anything, so it starts with 0
+        # what chain are we on?
+        # This get incremented before anything, so it starts with 0
+        self.chain_idx = -1
         self.nsamples = 0
-        assert nchains>0, "Must have > 0 chains specified (you sent %s)"%nchains
+        if nchains <= 0:
+            raise ValueError('MultipleChainMCMC: must have > 0 chains' +
+                             '(you sent %s)'.format(nchains))
 
-        self.chains = [self.make_sampler( make_h0, data, steps=steps/nchains, **kwargs) for _ in xrange(nchains)]
+        self.chains = [self.make_sampler(h0,
+                                         data,
+                                         steps=steps/nchains,
+                                         **kwargs)
+                       for _ in xrange(nchains)]
 
-    def make_sampler(self, make_h0, data, **kwargs):
+    def make_sampler(self, h0, data, **kwargs):
         """
-        This is called to make each of our internal samplers. It can be overwritten if you want something fnacy
-        :return:
+        make each an internal sampler.
+
+        It can be overwritten if you want something fnacy
+
+        Parameters
+        ----------
+        h0 : LOTlib.Hypotheses.Hypothesis
+            the initial hypothesis, used in each chain
+        data : list of data
+            what data we use
+        **kwargs :
+            special args to sampler
+
+        Returns
+        -------
+        LOTlib.Inference.Samplers.Sampler
+            a sampler to be used in one of the chains
         """
-        return MHSampler( make_h0(), data, **kwargs)
+        return MHSampler(h0, data, **kwargs)
 
     def __iter__(self):
         return self
@@ -54,26 +83,29 @@ class MultipleChainMCMC(Sampler):
 
     def acceptance_ratio(self):
         """
-            Return the mean acceptance rate of all chains
+        Return the acceptance rate of all chains
+
+        Returns
+        -------
+        list of floats
+            the acceptance rates of the chains
         """
         return [c.acceptance_ratio() for c in self.chains]
 
     def set_state(self, s, **kwargs):
         """
-        Set the state of this sampler. By necessity, we set the states of all samplers to copies. This is required when, for instance, we
-        next parallel tempering within PartitionMCMC
-        :param s: the state we set
+        Set the state of each chain in  sampler.
+
+        By necessity, we set the states of all samplers to copies. This is
+        required when, for instance, we parallel temper within PartitionMCMC.
+
+        Parameters
+        ----------
+        s : hypothesis
+            the state to which we set all the chains
+        **kwargs :
+            any additional arguments we want to pass to the chains' set_state
         """
         for c in self.chains:
-            c.set_state(copy(s), **kwargs) # it had better be a copy or all hell breaks loose
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-if __name__ == "__main__":
-    from LOTlib import break_ctrlc
-    from LOTlib.Examples.ExampleLoader import load_example
-
-    make_hypothesis, make_data = load_example('Number')
-    data = make_data(300)
-
-    for h in break_ctrlc(MultipleChainMCMC(make_hypothesis, data, steps=2000, nchains=10)):
-        print h.posterior_score, h
+            # copy s first, or all hell breaks loose
+            c.set_state(copy(s), **kwargs)
